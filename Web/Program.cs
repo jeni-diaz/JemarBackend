509ebb2abb540b133624ee99e5936a2c97160ad3
Jenifer Diaz <jenifergoogle@hotmail.com>
2026-04-12 19:08:22 -0300

    Agregar archivos de proyecto.

---
+++ Web/Program.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddApplicationPart(Assembly.Load("Presentation"));
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
