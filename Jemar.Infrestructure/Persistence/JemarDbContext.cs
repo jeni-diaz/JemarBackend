0e93fbb155d86f618ad85e63ee65626649c808f8
Jenifer Diaz <jenifergoogle@hotmail.com>
2026-05-03 14:26:22 -0300

    creamos el contecto... siiigan viendo

---
+++ Jemar.Infrestructure/Persistence/JemarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Jemar.Domain.Entities;
+
+namespace Jemar.Infrastructure.Persistence
+{
+    public class JemarDbContext : DbContext
+    {
+        public JemarDbContext(DbContextOptions<JemarDbContext> options) : base(options) 
+        { 
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
