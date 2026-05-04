b6f195fa203caf803b8d10137aaaf4043f9444d0
Martina Masanes <masanesmartina03@hotmail.com>
2026-05-04 15:31:17 -0300

    Cree entidades Shipment, ShipmentType, ShipmentStatus y sus enums

---
+++ Jemar.Domain/Entities/ShipmentStatus.cs
@@ -0,0 +1,11 @@
+﻿using Jemar.Domain.Enums;
+
+namespace Jemar.Domain.Entities
+{
+    public class ShipmentStatus
+    {
+        public int Id { get; set; }
+        public ShipmentStatusEnum Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
