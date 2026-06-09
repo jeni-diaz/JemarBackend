06ab5f3de0405c57d6346921de0768e5b4085964
Jenifer Diaz <jenifergoogle@hotmail.com>
2026-06-08 22:10:32 -0300

    cree la tabla role, para manejar los tipos de usuarios

---
+++ Jemar.Domain/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using Jemar.Domain.Enums;
+
+namespace Jemar.Domain.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public UserRole Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
